mjakeman/extension-manager

commit dcc312e75e7ac20ffe565011064d0d1062d6b83c

Make libbacktrace an optional dependency
libbacktrace is not available in some distributions where Extension
Manager is available as a native package, e.g. Gentoo [1], Debian [2].
On Debian, the native package maintainers have had to apply a downstream
patch [2] that removes and masks all code interfacing with libbacktrace.

However, when libbacktrace is available, the backtraces it helps to
collect in the event of an app crash might still be useful, so removing
the libbacktrace dependency completely is not the best solution for all
cases, i.e. not a good change to be submitted to the Extension Manager
upstream.

Making Extension Manager's dependency on libbacktrace optional is
better: it allows both these distributions to continue shipping this app
without downstream patching, and users who install this app via Flathub
or other distributions that support libbacktrace to report crashes with
helpful backtraces.

This commit introduces a new 'backtrace' Meson boolean option, which
controls whether Extension Manager is to be built with libbacktrace.  It
is set to 'true' by default, so existing build pipelines, e.g. the
first-party Flatpak builds for Flathub, behave the same as before.
Folks who wish to build Extension Manager without libbacktrace can do it
by setting '-Dbacktrace=false' in Meson command-line options.

[1]: https://gitweb.gentoo.org/repo/gentoo.git/commit/?id=c99e8159e6cb8705227aab759359c158f52b64bd
[2]: https://salsa.debian.org/gnome-team/gnome-shell-extension-manager/-/commit/a7cccc5486ebeca2597aeab2ea5d6c74b17488b0

Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

src/exm-backtrace.c
@@ -18,15 +18,21 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+#include "exm-config.h"
+
 #include "exm-backtrace.h"
 
 #include <glib.h>
 #include <stdint.h>
 
+#if WITH_BACKTRACE
 #include <backtrace-supported.h>
 #include <backtrace.h>
+#endif
 
+#if WITH_BACKTRACE
 static struct backtrace_state *state = NULL;
+#endif
 
 static void
 exm_backtrace_error_cb (void       *data,
@@ -54,18 +60,25 @@ exm_backtrace_full_cb (GString    *string_builder,
 void
 exm_backtrace_init (char *filename)
 {
-#ifdef BACKTRACE_SUPPORTED
+#if WITH_BACKTRACE
+
+# ifdef BACKTRACE_SUPPORTED
     state = backtrace_create_state (filename, 0,
                                     exm_backtrace_error_cb,
                                     NULL);
-#else
+# else
     g_warning ("Backtraces are not supported.\n");
+# endif
+
+#else
+    g_info ("Backtraces were not enabled at build time.\n");
 #endif
 }
 
 char *
 exm_backtrace_print ()
 {
+#if WITH_BACKTRACE
     GString *string_builder;
 
     if (!state)
@@ -82,4 +95,8 @@ exm_backtrace_print ()
                     string_builder);
 
     return g_string_free (string_builder, FALSE);
+#else
+    g_critical ("Backtraces were not enabled at build time.\n");
+    return NULL;
+#endif
 }

meson.build
@@ -25,6 +25,7 @@ config_h.set_quoted('PKG_NAME', get_option('package'))
 config_h.set_quoted('PKG_DISTRIBUTOR', get_option('distributor'))
 config_h.set10('IS_OFFICIAL', get_option('official'))
 config_h.set10('IS_DEVEL', get_option('development'))
+config_h.set10('WITH_BACKTRACE', get_option('backtrace'))
 configure_file(
   output: 'exm-config.h',
   configuration: config_h,

src/exm-application.c
@@ -201,11 +201,13 @@ exm_application_show_about (GSimpleAction *action,
                                         GTK_LICENSE_MPL_2_0,
                                         NULL);
 
+#if WITH_BACKTRACE
     adw_about_window_add_legal_section (ADW_ABOUT_WINDOW (about_window),
                                         "libbacktrace",
                                         "Copyright (C) 2012-2016 Free Software Foundation, Inc.",
                                         GTK_LICENSE_BSD_3,
                                         NULL);
+#endif
 
     adw_about_window_add_legal_section (ADW_ABOUT_WINDOW (about_window),
                                         "blueprint",

src/meson.build
@@ -29,7 +29,7 @@ exm_sources = [
 ]
 
 cc = meson.get_compiler('c')
-libbacktrace_dep = cc.find_library('backtrace', required: true)
+libbacktrace_dep = cc.find_library('backtrace', required: get_option('backtrace'))
 
 exm_deps = [
   dependency('gtk4'),
@@ -37,9 +37,12 @@ exm_deps = [
   dependency('gio-unix-2.0'),
   dependency('json-glib-1.0'),
   dependency('libsoup-3.0'),
-  dependency('text-engine-0.1'),
-  libbacktrace_dep
+  dependency('text-engine-0.1')
 ]
+
+if libbacktrace_dep.found()
+  exm_deps += libbacktrace_dep
+endif
 
 gnome = import('gnome')
 

commit f3768fb0227c714d7ed0613a28142613aa4a9c6b

backtrace: Define static functions only if `WITH_BACKTRACE`
The static functions are internal to file exm-backtrace.c and are only
used to facilitate backtrace collection.  In build configurations with
backtraces disabled ('meson setup -Dbacktrace=false'),
exm_backtrace_print() will not call the static functions, so they are
unused and thus need not be defined.

Previously, due to how the preprocessor macros were arranged, the static
variables in this file would not be defined when backtraces are
disabled; if any static functions use them, then builds with backtraces
disabled would fail due to a compiler error like:

    ../extension-manager-0.5.1/src/exm-backtrace.c: In function ‘exm_backtrace_full_cb’:
    ../extension-manager-0.5.1/src/exm-backtrace.c:58:9: error: ‘frames_omitted_count’ undeclared (first use in this function)
       58 |         frames_omitted_count++;
          |

This commit fixes such errors when backtraces are disabled.

Fixes: dcc312e (Make libbacktrace an optional dependency, 2023-06-03)
Fixes: d63d301 (backtrace: Collate 'null' messages, 2024-04-01)
Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

src/exm-backtrace.c
@@ -33,7 +33,6 @@
 #if WITH_BACKTRACE
 static struct backtrace_state *state = NULL;
 static int frames_omitted_count = 0;
-#endif
 
 static void
 exm_backtrace_error_cb (void       *data,
@@ -71,6 +70,7 @@ exm_backtrace_full_cb (GString    *string_builder,
 
     return 0;
 }
+#endif
 
 void
 exm_backtrace_init (char *filename)