045b2fa1635bb8f451be69656bab72140d752e1d
oscfdezdz <42654671+oscfdezdz@users.noreply.github.com>
2024-08-17 12:59:36 +0200

    browse-page: Remove unused header

--- src/exm-browse-page.h
+++ src/exm-browse-page.h
@@ -30,6 +30,5 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE (ExmBrowsePage, exm_browse_page, EXM, BROWSE_PAGE, GtkWidget)
 
 ExmBrowsePage *exm_browse_page_new (void);
-void exm_browse_page_refresh (ExmBrowsePage *self);
 
 G_END_DECLS
