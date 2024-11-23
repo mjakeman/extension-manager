3a1d19d5b549644606d79fb4e680e206651dd93c
oscfdezdz <42654671+oscfdezdz@users.noreply.github.com>
2024-11-23 16:17:16 +0100

    foo

--- src/local/regenerate-dbus.sh
+++ src/local/regenerate-dbus.sh
@@ -1,5 +1,5 @@
 gdbus-codegen --interface-prefix org.gnome.Shell. \
-	--generate-c-code shell-dbus-interface \
-	--c-namespace Shell \
-	--c-generate-object-manager \
-	org.gnome.Shell.Extensions.xml
+              --generate-c-code shell-dbus-interface \
+              --c-namespace Shell \
+              --c-generate-object-manager \
+              org.gnome.Shell.Extensions.xml
