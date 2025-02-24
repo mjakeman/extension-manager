7edefbef2f305ca9709be5c1a069cb78d45a72fb
Óscar Fernández Díaz <42654671+oscfdezdz@users.noreply.github.com>
2025-02-25 00:26:22 +0100

    search-result: Adapt to /search/{query} endpoint response

--- src/web/model/exm-search-result.h
+++ src/web/model/exm-search-result.h
@@ -1,3 +1,24 @@
+/*
+ * exm-search-result.h
+ *
+ * Copyright 2022-2025 Matthew Jakeman <mjakeman26@outlook.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #pragma once
 
 #include <glib-object.h>
@@ -9,8 +30,5 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE (ExmSearchResult, exm_search_result, EXM, SEARCH_RESULT, GObject)
 
 ExmSearchResult *exm_search_result_new (void);
-
-gboolean exm_search_result_supports_shell_version (ExmSearchResult *self,
-                                                   const gchar     *shell_version);
 
 G_END_DECLS
